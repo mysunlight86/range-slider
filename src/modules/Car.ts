175045e7469e04a1ed0215c08cafb08c66941d16
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-05-26 14:54:56 +0300

    Installed and configured eslint and the fsd plugin

--- src/modules/Car.ts
+++ src/modules/Car.ts
@@ -1,8 +1,23 @@
+/* eslint-disable class-methods-use-this */
 class Car {
   public go(msg: string): string {
+    // eslint-disable-next-line no-console
     console.log(msg);
     return msg;
   }
+
+  bindEventListeners() {
+    document.addEventListener('click', this.handleStopButtonClick);
+  }
+
+  handleStopButtonClick() {
+    // eslint-disable-next-line no-alert
+    alert('Спасибо!');
+  }
 }
 
-export { Car };
+// elem.addEventListener('click', function() {
+//   alert('Спасибо!');
+// });
+
+export default { Car };
