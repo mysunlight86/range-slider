1a60e74b6439c25f1b3e1efd2a242dfa5bedfd26
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-06-06 17:35:34 +0300

    Changed the scale to fit different step sizes, meaning it doesn't divide the range exactly

--- src/modules/scale.ts
+++ src/modules/scale.ts
@@ -2,7 +2,7 @@ class Scale {
   container = document.getElementById('range-slider');
   minValue = 0;
   maxValue = 300;
-  step = 50;
+  step = 137;
   mark = (this.maxValue - this.minValue) / this.step;
   // position = document.getElementById('tip-value').style.left;
   scale: HTMLElement;
@@ -13,15 +13,16 @@ class Scale {
   constructor() {
     this.scale = document.createElement('DIV');
     this.scale.classList.add('scale');
-    this.min = document.createElement('DIV');
+    this.min = document.createElement('SPAN');
     this.min.textContent = `${this.minValue}`;
-    this.min.style.position = 'relative';
-    this.min.style.left = '-4px';
+    this.min.style.position = 'absolute';
+    this.min.style.left = '24px';
     // this.min.style.border = '1px solid black';
-    this.max = document.createElement('DIV');
+    this.max = document.createElement('SPAN');
     this.max.textContent = `${this.maxValue}`;
-    this.max.style.position = 'relative';
-    this.max.style.left = '12px';
+    this.max.style.position = 'absolute';
+    this.max.style.left = '316px';
+    // this.max.style.left = `${Number(this.max.textContent) - 45}px`;
     // this.max.style.border = '1px solid black';
     // this.max.style.textAlign = 'right';
   }
@@ -33,19 +34,44 @@ class Scale {
     this.scale.append(this.max);
     console.log(this.max.offsetWidth);
     for (let i = 1; i < this.mark; i++) {
-      this.elem = document.createElement('DIV');
+      this.elem = document.createElement('SPAN');
       this.elem.textContent = `${this.step * i}`;
       // this.elem.style.border = '1px solid black';
-      this.elem.style.position = 'relative';
+      this.elem.style.position = 'absolute';
       // this.elem.style.left = `${this.elem.offsetLeft - 28}px`;
-      // this.elem.style.left = '20px';
+      // this.elem.style.left = '130px';
       // console.log(this.position);
       // this.elem.style.textAlign = 'center';
-      if (i === 1) {
+      /* if (i === 1) {
+        this.elem.style.left = `${Number(this.elem.textContent) - 20}px`;
+      } else {
+        this.elem.style.left = `${Number(this.elem.textContent) - 44}px`;
+      } */
+      if (Number(this.elem.textContent) < 100) {
+        this.elem.style.left = `${Number(this.elem.textContent) + 20}px`;
+      } else if (Number(this.elem.textContent) < 1000) {
+        this.elem.style.left = `${Number(this.elem.textContent) + 16}px`;
+      }
+      /* if (this.mark <= 3) {
+        this.elem.style.left = `${Number(this.elem.textContent) + 16}px`;
+      } else if (i === 1) {
+        this.elem.style.left = `${Number(this.elem.textContent) + 20}px`;
+      } else {
+        this.elem.style.left = `${Number(this.elem.textContent) + 16}px`;
+      } */
+      // if (i === 1) {
+      //   this.elem.style.left = `${Number(this.elem.textContent) - 16}px`;
+      // } else if (i === 2) {
+      //   this.elem.style.left = `${Number(this.elem.textContent) - 32}px`;
+      // } else {
+      //   this.elem.style.left = `${Number(this.elem.textContent) - 56}px`;
+      // }
+      // this.elem.style.left = `${Number(this.elem.textContent) - 20}px`;
+      /* if (i === 1) {
         this.elem.style.left = `${this.elem.offsetLeft + 8}px`;
       } else {
         this.elem.style.left = `${this.elem.offsetLeft + 12}px`;
-      }
+      } */
       this.max.insertAdjacentElement('beforebegin', this.elem);
     }
   }
